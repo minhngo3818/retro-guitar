459bcdee10202117a9cfd3f5bba090872504f61e
minhngo <tngo238@student.cccd.edu>
2022-04-18 13:30:11 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
